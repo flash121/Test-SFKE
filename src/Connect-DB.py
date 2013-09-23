c8fc5237b54310e6f2b264e281fc0f2d47608771
flash121 <gaoyfeng2010@hotmail.com>
2013-09-22 19:01:57 -0700

    Inital Project

---
+++ src/Connect-DB.py
@@ -0,0 +1,18 @@
+'''
+Created on 2013-9-22
+
+@author: yfeng
+
+Tags: MongoDB, Read- Like Expression in MongoDB, SELECT and Redistributed file
+
+Idea: Use MongoDB to read tag and redistributed store in the hardware (Space -> Time)
+
+'''
+
+from uploaddb import connect_c #connect mongodb socket: 
+
+
+self.old_db=connect_c(database,collection,host,port)
+
+
+
