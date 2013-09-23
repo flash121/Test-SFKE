fda261d6e4604348c60280aa1bc7d804dd605252
flash121 <gaoyfeng2010@hotmail.com>
2013-09-22 19:33:32 -0700

    Test Query Method

--- src/Connect-DB.py
+++ src/Connect-DB.py
@@ -7,12 +7,37 @@ Tags: MongoDB, Read- Like Expression in MongoDB, SELECT and Redistributed file
 
 Idea: Use MongoDB to read tag and redistributed store in the hardware (Space -> Time)
 
+Input: string, filename -> return every elements' index
 '''
+__author__ = 'Yifeng Gao'
+__email__ = 'gaoyfeng2010@hotmail.com'
 
 from uploaddb import connect_c #connect mongodb socket: 
 
 
-self.old_db=connect_c(database,collection,host,port)
+class KeyWordIndex():
+    def __init__(self,keyword='none',type='word'):
+        self.db=connect_c('facebook','train','localhost',27017)
+        if(type=='word'):
+            try:
+                self.keyword=keyword.split('')
+            except ValueError:
+                print "keyword Read failure from string"
+        if(type=='file'):
+            try:
+                self.keyword=open(keyword,'r').read().split('')
+            except ValueError:
+                print "keyword Read failure from file"
+    def Query(self):
+        print "Processing Query: "
+        f=open(out,'w')
+        docs=self.db.find({"Tags": {"$regex" : self.keyword}},{"Id" : 1})
+        for doc,k in enumerate(docs):
+            if k%1000==0:
+                print "Prcoessing "+k+"th Document" 
+            f.write(str(doc['Id'])+'\n')
+    
+    
 
 
 
