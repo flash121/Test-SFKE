8f5e297db16302530fa2064bb9503e2906c2e7b4
flash121 <gaoyfeng2010@hotmail.com>
2013-11-02 03:42:26 -0700

    Test Pass... Context Documents Generated

--- src/pre_processing.py
+++ src/pre_processing.py
@@ -7,19 +7,20 @@ Tags: Pre-processing raw data, eliminate stop-word and useless parts
 '''
 import logging
 import itertools
-import gensim
+from gensim import *
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', filename='web_book.log',level=logging.INFO)
 
 class webbook():
-    def __init__(self,dir='D:\\Stack Flow\\data',memory='friendly',maxitem=0):
+    def __init__(self,dir='D:\\Stack Flow\\data\\',memory='friendly',maxitem=0):
         self.directory=dir
         self.style=memory
         self.maxitem=maxitem
         self.len=0
         self.set=[open(dir+'1.dat','r'),open(dir+'2.dat','r'),open(dir+'3.dat','r'),open(dir+'4.dat','r'),open(dir+'5.dat','r'),open(dir+'6.dat','r')]
         self.loc=1
-        self.dict={"1.dat" : [],"2.dat" : [],"3.dat" : [],"4.dat" : [],"5.dat" : [],"6.dat" : []}
+        self.keyindcate=open("F:\\tags.csv",'r')
+        self.keyindcate.readline()
     def __getitem__(self):
         if(self.style=='friendly'):
             name=self.index_assign(self.doc[self.loc])
@@ -29,8 +30,6 @@ class webbook():
             name=self.index_assign(self.doc[self.loc])
             self.loc+=self.maxitem
             return open(name(0),'r')[name(1)]    
-        if(self.style=='heavy'):
-            shown_str=shown_str+'Data Directory: '+self.directory+'operation style: '+self.style+'maxmize item: '+'All files'
     def __str__(self):
         shown_str=''
         if(self.style=='friendly'):
@@ -43,6 +42,9 @@ class webbook():
     def __len__(self):
         return self.len
     def read_keyword(self,key):
+        """
+        read corresponse key_value data
+        """
         f=open('D:\\Stack Flow\\test\\key\\'+key+'.key')
         self.doc=f.read().split('\n')
         self.len=len(self.doc)
@@ -50,12 +52,47 @@ class webbook():
         '''
         index start from 1
         '''
-        return [(str(ins/1008000+1)+'.dat', ins%1008000) for ins in ind]
+        #return [(str(ins/1008000+1)+'.dat', ins%1008000) for ins in ind]
+        return str(ind/1008000+1)+'.dat', ind%1008000
     def back_jobs_translation(self):
         pass
-    def read(self):
+    def CreateBigTable(self):
+        """
+        Write Hash Table         key: id value: responsed word >_< using gensim
+        """
+        dict=corpora.Dictionary.load("D:\\Stack Flow\\data\\keyword.dict")
+        self.BigTable={}
+        for x in dict:
+            self.BigTable[dict[x]]=x
+            
+    def WriteBigFile(self):
+        """
+        New Idea: 0_0 hope Ok...  
+                  BigFile is a file document contains all the files (42000 files) with 20G space (so big...) 
+        """
+        for docs_guide in self.set:
+            for spec,doc in enumerate(docs_guide):
+                print spec
+                keywords=self.keyindcate.readline().replace("\"","").split();
+                for word in keywords:
+                    f=open(self.directory+"kdoc\\"+str(self.BigTable[word])+".kdoc",'a+')
+                    f.write(doc+" ")
+                    f.close()
+    def CleanWord(self):
+"""
+        Work: 2013/9  Forget >_< 
         name=self.index_assign(self.doc[self.loc])
         self.loc+=1
         return open(name(0),'r')[name(1)]
-    
-    
+        """
+"""
+Test Case: (CreateBigTable)
+"""    
+x=webbook();
+print "Start Creating Big Table"
+x.CreateBigTable();
+#print "Successful"
+"""
+Test Case: (WriteBigFile)
+"""    
+#x.WriteBigFile()
